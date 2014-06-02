6657b0b15fd8348001e6b28aaff296f5886dd518
poornima <pvenkatakrishnan@paypal.com>
2014-06-02 16:40:56 -0700

    Implementing overriding the express view class as a middleware

--- view/js.js
+++ view/js.js
@@ -17,73 +17,30 @@
  \*───────────────────────────────────────────────────────────────────────────*/
 'use strict';
 
-var path = require('path'),
-    fs = require('graceful-fs'),
-    resolver = require('file-resolver');
+var fs = require('graceful-fs');
+var resolver = require('file-resolver');
 
+exports.create = function (config) {
 
-var proto = {
-
-    get path() {
-        // Unfortunately, since we don't know the actual file to resolve until
-        // we get request context (in `render`), we can't say whether it exists or not.
-        return true;
-    },
-
-    render: function (options, callback) {
-        var locals, view, engine;
-
-        locals = options && options.context;
-        view = this.resolver.resolve(this.name, locals && locals.locality);
-
-        // This is a bit of a hack to ensure we override `views` for the duration
-        // of the rendering lifecycle. Unfortunately, `adaro` and `consolidate`
-        // (https://github.com/visionmedia/consolidate.js/blob/407266806f3a713240db2285527de934be7a8019/lib/consolidate.js#L214)
-        // check `options.views` but override with `options.settings.views` if available.
-        // So, for this rendering task we need to override with the more specific root directory.
-        options.settings = Object.create(options.settings);
-        options.views = options.settings.views = view.root;
-
-        engine = this.engines['.' + this.defaultEngine];
-        engine(view.file, options, callback);
-    }
-
-};
-
-
-function buildCtor(fallback) {
-
-    function View(name, options) {
-        this.name = name;
-        this.root = options.root;
-        this.defaultEngine = options.defaultEngine;
-        this.engines = options.engines;
-        this.resolver = resolver.create({ root: options.root, ext: this.defaultEngine, fallback: fallback });
-    }
-
-    View.prototype = proto;
-
-    return View;
-
-}
-
-
-exports.create = function (config, app) {
     var res,
         defaultLocale = config.i18n.fallback;
+
     res = resolver.create({ root: config.views, ext: 'js', fallback: defaultLocale });
-    app.set('view', buildCtor(defaultLocale));
 
     return function onLoad(name, context, callback) {
         var locals, view;
 
         locals = context.get('context');
+
         view = res.resolve(name, locals && locals.locality);
+        console.info('the locale rendered in:', locals.locality);
+        console.info('the view rendered:', view.file);
         if (!view.file) {
             callback(new Error('Could not load template ' + name));
             return;
         }
         fs.readFile(view.file, 'utf8', callback);
     };
+
 };
 
