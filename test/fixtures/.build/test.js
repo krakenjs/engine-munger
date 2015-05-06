62624e3597802e1082e49d8a01603ddd3a0a2527
Aria Stewart <aredridel@dinhe.net>
2015-05-06 16:05:44 -0400

    Update built fixtures to use dust 2.7 format

--- test/fixtures/.build/test.js
+++ test/fixtures/.build/test.js
@@ -1,1 +1,1 @@
-(function(){dust.register("test",body_0);function body_0(chk,ctx){return chk.write("<h1>Hey Test</h1>");}return body_0;})()
+(function(dust){dust.register("test",body_0);function body_0(chk,ctx){return chk.w("<h1>Hey Test</h1>");}body_0.__dustBody=!0;return body_0}(dust));
