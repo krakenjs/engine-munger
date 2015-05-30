c0ec0b69bd3698c74c3172f6c50085f500be0dc3
Aria Stewart <aredridel@dinhe.net>
2015-05-29 23:20:55 -0400

    Update tests to use expressView, rather than the engine directly

--- test/index.js
+++ test/index.js
@@ -1,18 +1,15 @@
 'use strict';
-var test = require('tap').test,
-    engineMunger = require('../index'),
-    testData = require('./fixtures/testConfig'),
-    freshy = require('freshy');
-
+var test = require('tap').test;
+var engineMunger = require('../index');
+var freshy = require('freshy');
+var makeViewClass = require('../lib/expressView').makeViewClass;
+var path = require('path');
 
 test('engine-munger', function (t) {
     var settings = {cache: false};
 
     t.test('when no specialization or internationalization enabled for js engine', function (t) {
-        var settings = {cache: false},
-            config = testData['none-js'].config;
-
-        engineMunger.js(settings, config)('test', {views: 'test/fixtures/.build'}, function(err, data) {
+        makeView('js', 'test', {}).render({}, function(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>Hey Test</h1>');
             t.end();
@@ -20,14 +17,22 @@ test('engine-munger', function (t) {
     });
 
     t.test('when only specialization enabled for js engine', function (t) {
-        var config = testData['onlySpcl-js'].config;
-        var context1 = testData['onlySpcl-js'].context1;
-        var context2 = testData['onlySpcl-js'].context2;
-        var engine = engineMunger.js(settings, config);
-        engine('spcl/jekyll', context1, function(err, data) {
+        var conf = {
+            specialization: {
+                'spcl/jekyll': [
+                    {
+                        is: 'spcl/hyde',
+                        when: {
+                            'whoAmI': 'badGuy'
+                        }
+                    }
+                ]
+            }
+        }
+        makeView('js', 'spcl/jekyll', conf).render({ whoAmI: 'badGuy' }, function(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>☃greeting☃ Hyde</h1>');
-            engine('spcl/jekyll', context2, function(err, data) {
+            makeView('js', 'spcl/jekyll', conf).render({ whoAmI: 'goodGuy' }, function(err, data) {
                 t.equal(err, null);
                 t.equal(data, '<h1>☃greeting☃ Jekyll</h1>');
                 t.end();
@@ -36,9 +41,18 @@ test('engine-munger', function (t) {
     });
 
     t.test('when only internationalization enabled for js engine', function (t) {
-        var config = testData['onlyIntl-js'].config,
-            context = testData['onlyIntl-js'].context;
-        engineMunger.js(settings, config)('jekyll', context, function(err, data) {
+        var config = {
+            i18n: {
+                fallback: 'en-US',
+                formatPath: krakenFormatPath,
+                contentPath: 'test/fixtures/properties'
+            }
+        };
+        var context = {
+            locale: 'es_US'
+        };
+
+        makeView('js', 'jekyll', config).render(context, function(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>Hola Don Jekyll</h1>');
             t.end();
@@ -47,30 +61,63 @@ test('engine-munger', function (t) {
     });
 
     t.test('when specialization and internationalization enabled for js engine', function (t) {
-        var config = testData['spclAndIntl-js'].config;
-        var context1 = testData['spclAndIntl-js'].context1;
-        var context2 = testData['spclAndIntl-js'].context2;
-        var context3 = testData['spclAndIntl-js'].context3;
-        var context4 = testData['spclAndIntl-js'].context4;
-        var engine = engineMunger.js(settings, config);
-        engine('spcl/jekyll', context1, checkContext1);
+        var config = {
+            i18n: {
+                fallback: 'en-US',
+                formatPath: krakenFormatPath,
+                contentPath: 'test/fixtures/properties'
+            },
+            specialization: {
+                'spcl/jekyll': [
+                    {
+                        is: 'spcl/hyde',
+                        when: {
+                            'whoAmI': 'badGuy'
+                        }
+                    }
+                ]
+
+            }
+        };
+
+        var context1 = {
+            whoAmI: 'badGuy',
+            locale: 'es_US'
+        };
+
+        var context2 = {
+            whoAmI: 'goodGuy',
+            locale: 'es_US'
+        };
+
+        var context3 = {
+            whoAmI: 'badGuy',
+            locale: 'en_US'
+        };
+
+        var context4 = {
+            whoAmI: 'goodGuy',
+            locale: 'en_US'
+        };
+
+        makeView('js', 'spcl/jekyll', config).render(context1, checkContext1);
 
         function checkContext1(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>Hola Señor Hyde</h1>');
-            engine('spcl/jekyll', context2, checkContext2);
+            makeView('js', 'spcl/jekyll', config).render(context2, checkContext2);
         }
 
         function checkContext2(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>Hola Don Jekyll</h1>');
-            engine('spcl/jekyll', context3, checkContext3);
+            makeView('js', 'spcl/jekyll', config).render(context3, checkContext3);
         }
 
         function checkContext3(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>Hello Mister Hyde</h1>');
-            engine('spcl/jekyll', context4, checkContext4);
+            makeView('js', 'spcl/jekyll', config).render(context4, checkContext4);
         };
 
         function checkContext4(err, data) {
@@ -83,9 +130,9 @@ test('engine-munger', function (t) {
 
     t.test('when no specialization or internationalization enabled for dust engine', function (t) {
 
-        var config = testData['none-dust'].config;
+        var config = {};
 
-        engineMunger.dust(settings, config)('test', {views: 'test/fixtures/templates'}, function(err, data) {
+        makeView('dust', 'test', {}).render({}, function(err, data) {
             t.equal(err, null);
             t.equal(data, '<h1>Hey Test</h1>');
             t.end();
@@ -95,122 +142,60 @@ test('engine-munger', function (t) {
 
     t.test('using munger when only specialization enabled for dust engine', function (t) {
 
-        var config = testData['onlySpcl-dust'].config,
-            context = testData['onlySpcl-dust'].context;
+        var config = {
+             specialization: {
+                'spcl/jekyll': [
+                    {
+                        is: 'spcl/hyde',
+                        when: {
+                            'whoAmI': 'badGuy'
+                        }
+                    }
+                ]
+            }
+        };
 
-        engineMunger.dust(settings, config)('spcl/jekyll', context, function (err, data) {
+        var context = { 'whoAmI': 'badGuy' };
+
+        makeView('dust', 'spcl/jekyll', config).render(context, function (err, data) {
             t.equal(err, null);
             t.equal(data, '<h1> Hyde</h1>');
             t.end();
         });
 
-    });
-
-    t.test('using munger when only specialization enabled for dust engine with cache', function (t) {
-
-        var config = testData['onlySpcl-dust'].config,
-            context = testData['onlySpcl-dust'].context,
-            setting = {cache: true};
-
-        engineMunger.dust(setting, config)('spcl/jekyll', context, function (err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1> Hyde</h1>');
-            t.end();
-        });
-
-    });
-
-    t.test('when only internationalization is enabled for dust engine', function (t) {
-        var config = testData['onlyIntl-dust'].config,
-            context = testData['onlyIntl-dust'].context;
-        engineMunger.dust(settings, config)('jekyll', context, function(err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1>Hola Don Jekyll</h1>');
-            t.end();
-        });
-
-    });
-
-
-    t.test('when specialization/internationalization is enabled for dust engine', function(t) {
-        var config = testData['spclAndIntl-dust'].config,
-            context = testData['spclAndIntl-dust'].context;
-
-        engineMunger.dust(settings, config)('spcl/jekyll', context, function(err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1>Hola Señor Hyde</h1>');
-            t.end();
-        });
-    });
-
-    t.test('when specialization/internationalization is enabled for dust engine with cache', function(t) {
-        var config = testData['spclAndIntl-dust'].config,
-            context = testData['spclAndIntl-dust'].context,
-            settings = {cache: true};
-
-        engineMunger.dust(settings, config)('spcl/jekyll', context, function(err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1>Hola Señor Hyde</h1>');
-            t.end();
-        });
-    });
-
-
-    t.test('i18n using view.render for js engine', function(t) {
-        var config = testData['onlyIntl-js'].config,
-            context = testData['onlyIntl-js'].context;
-        var engine = engineMunger.js(settings, config);
-
-        engineMunger.js(settings, config)('jekyll', context, function(err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1>Hola Don Jekyll</h1>');
-            t.end();
-        });
-    });
-
-    t.test('i18n using view.render for js engine with caching', function(t) {
-        var config = testData['onlyIntl-js'].config,
-            context = testData['onlyIntl-js'].context,
-            settings = {cache: true};
-        var engine = engineMunger.js(settings, config);
-
-        engineMunger.js(settings, config)('jekyll', context, function(err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1>Hola Don Jekyll</h1>');
-            t.end();
-        });
-    });
-
-    t.test('when specialization/internationalization is enabled for dust engine with cache', function(t) {
-        var config = testData['spclAndIntl-dust'].config,
-            context = testData['spclAndIntl-dust'].context,
-            settings = {cache: true};
-
-        engineMunger.dust(settings, config)('spcl/jekyll', context, function(err, data) {
-            t.equal(err, null);
-            t.equal(data, '<h1>Hola Señor Hyde</h1>');
-            t.end();
-        });
     });
 
     //error cases
     t.test('i18n with js engine- template not found case', function(t) {
-        var config = testData['onlyIntl-js'].config,
-            context = testData['onlyIntl-js'].context;
-        engineMunger.js(settings, config)('peekaboo', context, function(err, data) {
-            t.equal(err.message, 'Could not load template peekaboo');
+        var config = {
+            i18n: {
+                fallback: 'en-US',
+                formatPath: krakenFormatPath,
+                contentPath: 'test/fixtures/properties'
+            }
+        };
+        makeView('js', 'peekaboo', config).render({}, function(err, data) {
+            t.ok(/Failed to lookup view "peekaboo" in views directory/.test(err.message));
             t.equal(data, undefined);
             t.end();
         });
-
     });
 
 
     t.test('i18n dust engine- catch error while compiling invalid dust and report name of broken template', function(err, data) {
+        var config = {
+            i18n: {
+                fallback: 'en-US',
+                formatPath: krakenFormatPath,
+                contentPath: 'test/fixtures/properties'
+            }
+        };
 
-        var config = testData['onlyIntl-dust'].config,
-            context = testData['onlyIntl-dust'].context;
-        engineMunger.dust(settings, config)('invalidTemp', context, function(err, data) {
+        var context = {
+            locale: 'es_US'
+        };
+
+        makeView('dust', 'invalidTemp', config).render(context, function(err, data) {
             t.equal(err.message, 'Problem rendering dust template named invalidTemp: Expected end tag for elements but it was not found. At line : 5, column : 11');
             t.equal(data, undefined);
             t.end();
@@ -219,3 +204,20 @@ test('engine-munger', function (t) {
     });
 
 });
+
+function makeView(ext, tmpl, config) {
+    var viewConf = {};
+    viewConf[ext] = config;
+    var View = makeViewClass(viewConf);
+    var engines = {};
+    engines['.' + ext] = engineMunger[ext]();
+    return new View(tmpl, {
+        root: ext == 'js' ? path.resolve(__dirname, 'fixtures/.build') : path.resolve(__dirname, 'fixtures/templates'),
+        defaultEngine: '.' + ext,
+        engines: engines
+    });
+}
+
+function krakenFormatPath(locale) {
+    return locale.langtag.region + '/' + locale.langtag.language.language;
+}
